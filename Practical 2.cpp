f11e9c95d69f12c113d02884acbb5640a682673d
viraj-1111 <virajnikam2025.etc@mmcoe.edu.in>
2026-09-10 23:03:32 +0530

    Update Practical 2.cpp

--- Practical 2.cpp
+++ Practical 2.cpp
@@ -1,16 +1,20 @@
-# include<iostream>
+#include <iostream>
 using namespace std;
+
 int main()
-{
-    int i, even=0, odd=0;
-    for(i=0;i<=50;i++)
-    {
-        if(i%2==0)
-        even=even+i;
-        else
-        odd=odd+i;
+{ int i,sum=0;
+    for(i=1;i<=50;i++)
+    {if(i%2==0)
+     sum+=i;
     }
-    cout<<"sum of even no.= "<<even<<endl;
-    cout<<"sum of odd no.= "<<odd<<endl;
+    cout<< "sum of all even numbers from 1 to 50 is:"<<sum<<endl;
+    sum=0;
+     for(i=1;i<=50;i++)
+    {if(i%2!=0)
+     sum+=i;
+    }
+    cout<< "sum of all odd numbers from 1 to 50 is:"<<sum<<endl;
+   
+
     return 0;
 }
